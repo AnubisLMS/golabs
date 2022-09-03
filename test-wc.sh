6ffd1b58cd5adf20d8d73b320b18bc622c4bd9d0
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-09-03 18:55:56 -0400

    CHG adjust return codes

--- test-wc.sh
+++ test-wc.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 go run wc.go master sequential txt/pg-*.txt
 sort -n -k2 mrtmp.wcseq | tail -10 | diff - txt/mr-testout.txt > diff.out
+
+cat txt/mr-testout.txt
+
 if [ -s diff.out ]
 then
-echo "Failed test. Output should be as in mr-testout.txt. Your output differs as follows (from diff.out):" > /dev/stderr
-  cat diff.out
+    echo "Failed test. Output should be as in mr-testout.txt. Your output differs as follows (from diff.out):" > /dev/stderr
+    cat diff.out
+    exit 1
 else
-  echo "Passed test" > /dev/stderr
+    echo "Passed test" > /dev/stderr
+    exit 0
 fi
 
